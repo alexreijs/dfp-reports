74e1a6ead89f92622b26454c3e2a4b5b4ccf9628
Alex Reijs <alexander.reijs@sanoma.com>
2016-02-10 11:36:10 +0000

    Initial commit

---
+++ AdOperationsLineItems.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * This example gets all line items. To create line items, run
+ * CreateLineItems.php.
+ *
+ * PHP version 5
+ *
+ * Copyright 2014, Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    GoogleApiAdsDfp
+ * @subpackage v201508
+ * @category   WebServices
+ * @copyright  2014, Google Inc. All Rights Reserved.
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License,
+ *             Version 2.0
+ */
+error_reporting(E_STRICT | E_ALL);
+
+// You can set the include path to src directory or reference
+// DfpUser.php directly via require_once.
+// $path = '/path/to/dfp_api_php_lib/src';
+$path = dirname(__FILE__) . '/lib';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+require_once 'Google/Api/Ads/Dfp/Lib/DfpUser.php';
+require_once 'Google/Api/Ads/Dfp/Util/v201508/StatementBuilder.php';
+require_once dirname(__FILE__) . '/examples/Common/ExampleUtils.php';
+
+try {
+  // Get DfpUser from credentials in "../auth.ini"
+  // relative to the DfpUser.php file's directory.
+  $user = new DfpUser();
+
+  // Log SOAP XML request and response.
+  $user->LogDefaults();
+
+  // Get the LineItemService.
+  $lineItemService = $user->GetService('LineItemService', 'v201508');
+
+  // Create a statement to select all line items.
+  $statementBuilder = new StatementBuilder();
+  $statementBuilder->OrderBy('id ASC')
+      ->Limit(StatementBuilder::SUGGESTED_PAGE_LIMIT);
+
+  $statementBuilder->Where("StartDateTime >= '" . date('Y-m-d', time() - 86400 * 31) . "'");
+
+  // Default for total result set size.
+  $totalResultSetSize = 0;
+
+  printf("Saving results to file\n");
+
+  $fn = "./line-items.csv";
+  $fp = fopen($fn, "a");
+
+  do {
+    // Get line items by statement.
+    $page = $lineItemService->getLineItemsByStatement(
+        $statementBuilder->ToStatement());
+
+    // Save results.
+
+
+    if (isset($page->results)) {
+
+      $totalResultSetSize = $page->totalResultSetSize;
+      $i = $page->startIndex;
+      foreach ($page->results as $lineItem) {
+	$line = implode(',', array($lineItem->id, $lineItem->orderId)) . "\n";
+	fwrite($fp, $line);
+      }
+
+    }
+
+    $statementBuilder->IncreaseOffsetBy(StatementBuilder::SUGGESTED_PAGE_LIMIT);
+  } while ($statementBuilder->GetOffset() < $totalResultSetSize);
+
+  fclose($fp);
+
+  shell_exec("cp line-items.csv " . sprintf('%s.csv', 'line-items-' . date('m-d-Y_H-i')));
+
+  printf("Uploading to Google Storage\n");
+  $shell = shell_exec("/usr/local/bin/gsutil cp line-items*.csv gs://api-hub-output/dfp-reports/ad-operations/line-items/");
+
+  printf("Deleting downloaded files\n");
+  shell_exec("rm line-items*.csv");
+
+
+  printf("Number of results found: %d\n", $totalResultSetSize);
+} catch (OAuth2Exception $e) {
+  ExampleUtils::CheckForOAuth2Errors($e);
+} catch (ValidationException $e) {
+  ExampleUtils::CheckForOAuth2Errors($e);
+} catch (Exception $e) {
+  printf("%s\n", $e->getMessage());
+}
+
+
---
+++ ReachReport.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This example runs a reach report.
+ *
+ * PHP version 5
+ *
+ * Copyright 2014, Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    GoogleApiAdsDfp
+ * @subpackage v201508
+ * @category   WebServices
+ * @copyright  2014, Google Inc. All Rights Reserved.
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License,
+ *             Version 2.0
+ */
+error_reporting(E_STRICT | E_ALL);
+
+date_default_timezone_set('Europe/Amsterdam');
+
+// You can set the include path to src directory or reference
+// DfpUser.php directly via require_once.
+// $path = '/path/to/dfp_api_php_lib/src';
+$path = dirname(__FILE__) . '/lib';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+require_once 'Google/Api/Ads/Dfp/Lib/DfpUser.php';
+require_once 'Google/Api/Ads/Dfp/Util/v201508/ReportDownloader.php';
+require_once 'Google/Api/Ads/Dfp/Util/v201508/StatementBuilder.php';
+require_once dirname(__FILE__) . '/examples/Common/ExampleUtils.php';
+
+try {
+  // Get DfpUser from credentials in "../auth.ini"
+  // relative to the DfpUser.php file's directory.
+  $user = new DfpUser();
+
+  // Log SOAP XML request and response.
+  $user->LogDefaults();
+
+  // Get the ReportService.
+  $reportService = $user->GetService('ReportService', 'v201508');
+
+  // Create report query.
+  $reportQuery = new ReportQuery();
+  $reportQuery->dimensions = array('ORDER_ID');
+  $reportQuery->columns = array('REACH_FREQUENCY', 'REACH');
+
+  // Set the dynamic date range type or a custom start and end date that is
+  // the beginning of the week (Sunday) to the end of the week (Saturday), or
+  // the first of the month to the end of the month.
+  $reportQuery->dateRangeType = 'REACH_LIFETIME';
+
+  // Create report job.
+  $reportJob = new ReportJob();
+  $reportJob->reportQuery = $reportQuery;
+
+  // Run report job.
+  $reportJob = $reportService->runReportJob($reportJob);
+
+  // Create report downloader.
+  $reportDownloader = new ReportDownloader($reportService, $reportJob->id);
+
+  printf("Waiting for report to be ready ...\n");
+  
+  // Wait for the report to be ready.
+  $reportDownloader->waitForReportReady();
+
+  // Change to your file location.
+  $filePath = sprintf('%s.csv.gz', 'reach-report-' . date('m-d-Y_H-i'));
+
+  printf("Downloading report to %s ...\n", $filePath);
+
+  // Download the report.
+  $reportDownloader->downloadReport('CSV_DUMP', $filePath);
+  
+  // Unzip report
+  $unzipped = gzdecode(file_get_contents($filePath));
+  printf("Unzipping report...\n", $filePath);
+
+  
+  // Save including date
+  $fn = substr($filePath, 0, -3);
+  $fp = fopen($fn, "a");
+  fwrite($fp, $unzipped);
+  fclose($fp);
+
+  // Save excluding date
+  $fn = "./reach-report.csv";
+  $fp = fopen($fn, "a");
+  fwrite($fp, $unzipped);
+  fclose($fp);
+
+
+  printf("Uploading to Google Storage\n");
+
+  $shell = shell_exec("/usr/local/bin/gsutil cp reach-report*.csv gs://api-hub-output/dfp-reports/reach-report/");
+  //foreach($shell as $id => $sh) {
+  //  printf($sh);
+  //}
+
+  printf("Deleting downloaded files\n");
+  shell_exec("rm reach-report*.csv & rm reach-report*.csv.gz");
+
+  
+  printf("Done.\n");
+  
+} catch (OAuth2Exception $e) {
+  ExampleUtils::CheckForOAuth2Errors($e);
+} catch (ValidationException $e) {
+  ExampleUtils::CheckForOAuth2Errors($e);
+} catch (Exception $e) {
+  printf("%s\n", $e->getMessage());
+}
+
